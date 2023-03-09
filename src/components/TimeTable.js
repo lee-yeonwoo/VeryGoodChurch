c620328c7bebaaf5bb1568c8e54ae6117403723d
yeonwoo <y8551472@gmail.com>
2023-03-09 17:15:27 +0900

    styled component 적용

--- src/components/TimeTable.js
+++ src/components/TimeTable.js
@@ -13,11 +13,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('주일예배', '오전9시', '본당', 'ㅇㅇㅇ목사님'),
+  createData('수요예배', '오전9시', '본당', 'ㅇㅇㅇ목사님'),
+  createData('금요철야', '오전9시', '본당', 'ㅇㅇㅇ목사님'),
+  createData('어린이예배', '오전9시', '본당', 'ㅇㅇㅇ목사님'),
+  createData('새벽예배', '오전9시', '본당', 'ㅇㅇㅇ목사님'),
 ];
 
 export default function TimeTable() {
@@ -26,11 +26,10 @@ export default function TimeTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>구분</TableCell>
+            <TableCell align="right">예배시간</TableCell>
+            <TableCell align="right">예배장소</TableCell>
+            <TableCell align="right">담당교역자</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -45,7 +44,6 @@ export default function TimeTable() {
               <TableCell align="right">{row.calories}</TableCell>
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
