11940d097f32fdd2707527e21481f78da40c045a
yeonwoo <y8551472@gmail.com>
2023-03-24 18:42:42 +0900

    페이지네이션 구현중

--- src/pages/ChurchLife.jsx
+++ src/pages/ChurchLife.jsx
@@ -1,31 +1,55 @@
 import React from "react";
+import { useState,useEffect } from "react";
 import styled from "styled-components";
 import AppBar from "../components/AppBar";
 import Footer from "../components/Footer";
 import TitleBox from '../components/TitleBox';
-import Card from "../components/Card";
 
+import Pagination from "../components/pagination";
+// import sampleImg from "../sampleImg.png";
 
 
 const ChurchLife = () => {
-  const titles = ["title1","title2","title3","title4","title5","title6","title7","title8","title8"]
-    const titleList = titles.map((title)=>(<Card title={title}/>))
+  //useState()를 먼저 선언해줍니다
+  const [photos,setPosts] = useState([]); //초기값은 빈배열로..왜인지는 ...뭔가 빈 것 부터 시작해도되니까여
+  const limit = 12;
+  const [page,setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
+  //useEffect()로 api를 불러오는 거 !! 비동기로 처리하는데 사용되는 기능이랄까요
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
     return (
       <>
       <AppBar />
       <WrapBox>
       <TitleBox miniTitle = "교회생활"></TitleBox>
-      <CardListBoxDiv>
 
-      {
-            titleList.map((a, i) => {
-              return <Card title={a} num={i} key={i}/>
-              // return <Card products={products[i]} num={i} key={i}/>
-            })
-          }
-        
-        
+      <label>
+        페이지 당 표시할 게시물 수:&nbsp;
+     
+      </label>
+      <CardListBoxDiv>
+        {
+        photos.slice(offset,offset+limit).map(({ id,title,body}) => (
+            <CardDiv key = {id}>
+          <TitleImg>{body}</TitleImg>
+          <Title>{id}. {title}</Title>
+          <Line></Line>
+        </CardDiv>
+          ))}
       </CardListBoxDiv>
+      <number>
+        <Pagination
+        total={photos.length}
+        length={limit}
+        page={page}
+        setPage={setPage}
+        />
+      </number>
       <Footer />
       </WrapBox>
       </>
@@ -35,6 +59,42 @@ const ChurchLife = () => {
   const WrapBox = styled.div`
     width: 100%;
   `;
+
+const CardDiv = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+position: relative;
+width: 30%;
+height: 30%;
+margin-bottom: 20px;
+background-color: #dddddd;
+cursor: pointer;
+
+`
+
+const TitleImg = styled.div`
+position: absolute;
+width: 100%;
+height: 100%;
+filter : brightness(70%); /* brightness: 이미지 살짝 어둡게 */
+`
+
+const Title = styled.label`
+position: absolute;
+color: white;
+z-index: 1000;
+font-weight: bold;
+font-size: 100%;
+
+`
+const Line = styled.div `
+width: 80%;
+height: 80%;
+position: absolute;
+border: 3px solid white;
+
+`
   const CardListBoxDiv = styled.div`
   height: 100vh;
   display: flex;
